c53fbb2f4a0e97473b3661a78768d1d39c7dc732
Ivan Nikitin <vantoozz@gmail.com>
2013-06-17 17:29:02 +0400

    + call

--- src/Forecast.php
+++ src/Forecast.php
@@ -3,9 +3,30 @@
 class Forecast{
 	
 	private $api_key;
-	
-	public function __construct($api_key){
+
+    private $endpoint = 'https://api.forecast.io/forecast/';
+
+    public $descriptions = array(
+        'clear-day',
+        'clear-night',
+        'rain',
+        'snow',
+        'sleet',
+        'wind',
+        'fog',
+        'cloudy',
+        'partly-cloudy-day',
+        'partly-cloudy-night',
+    );
+
+    public function __construct($api_key){
 		 $this->api_key=$api_key;
 	}
-		
+
+
+    public function call($latitude, $longitude, $time = null, $exclude = null, $units = 'si'){
+        $url = $this->endpoint . $this->api_key . '/' . $latitude . ',' . $longitude . ($time ? ',' . $time : '') . '?units=' . $units . ($exclude ? '&exclude=' . $exclude : '');
+        return json_decode(file_get_contents($url));
+    }
+
 }
