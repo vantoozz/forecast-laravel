d4f5e4fe7352c5f1f7cfd956790f79852036b07b
Ivan Nikitin <vantoozz@gmail.com>
2013-06-17 16:21:36 +0400

    initial commit

---
+++ src/Forecast.php
@@ -0,0 +1,11 @@
+<?php
+
+class Forecast{
+	
+	private $api_key;
+	
+	public function __construct($api_key){
+		 $this->api_key=$api_key;
+	}
+		
+}
---
+++ src/ForecastException.php
@@ -0,0 +1,6 @@
+<?php
+
+class ForecastException extends Exception
+{
+
+}
